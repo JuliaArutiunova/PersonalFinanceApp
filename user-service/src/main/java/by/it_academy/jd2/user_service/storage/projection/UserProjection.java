334b785bb90161a0a6a78e18c30360f017f64973
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-11-18 19:30:28 +0300

    UserProjection: Timestamp исправлен на LocalDateTime

--- user-service/src/main/java/by/it_academy/jd2/user_service/storage/projection/UserProjection.java
+++ user-service/src/main/java/by/it_academy/jd2/user_service/storage/projection/UserProjection.java
@@ -3,15 +3,15 @@ package by.it_academy.jd2.user_service.storage.projection;
 import by.it_academy.jd2.user_service.storage.entity.UserRole;
 import by.it_academy.jd2.user_service.storage.entity.UserStatus;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface UserProjection {
     UUID getUserId();
 
-    Timestamp getDtCreate();
+    LocalDateTime getDtCreate();
 
-    Timestamp getDtUpdate();
+    LocalDateTime getDtUpdate();
 
     String getMail();
 
