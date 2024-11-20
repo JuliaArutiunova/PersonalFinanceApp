29a77af00ab30a31177b0fbf83bd8bf58b4dce2c
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-11-20 12:33:06 +0300

    Добавлены аннотации Async и EnableAsync для отправки почты, добавлен конструктор в MailSenderService

--- user-service/src/main/java/by/it_academy/jd2/user_service/UserServiceApplication.java
+++ user-service/src/main/java/by/it_academy/jd2/user_service/UserServiceApplication.java
@@ -5,10 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableConfigurationProperties({JWTProperty.class})
+@EnableTransactionManagement
+@EnableAsync
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
--- user-service/src/main/java/by/it_academy/jd2/user_service/service/MailSenderService.java
+++ user-service/src/main/java/by/it_academy/jd2/user_service/service/MailSenderService.java
@@ -1,20 +1,25 @@
 package by.it_academy.jd2.user_service.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MailSenderService {
 
     @Value("${spring.mail.username}")
-    private String from;
+    private  String from;
     private String title = "PersonalFinances верификация";
-    @Autowired
-    private JavaMailSender javaMailSender;
 
+    private final JavaMailSender javaMailSender;
+
+    public MailSenderService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Async
     public void sendMail(String to, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
