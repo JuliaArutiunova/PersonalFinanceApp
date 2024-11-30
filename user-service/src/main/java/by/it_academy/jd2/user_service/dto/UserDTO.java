ce13d6a9c7ae5757b26f4e06ebf0c3baf825ee65
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-11-30 20:20:25 +0300

    UserDTO: Исправлено имя поля id, добавлен конструктор

--- user-service/src/main/java/by/it_academy/jd2/user_service/dto/UserDTO.java
+++ user-service/src/main/java/by/it_academy/jd2/user_service/dto/UserDTO.java
@@ -1,20 +1,22 @@
 package by.it_academy.jd2.user_service.dto;
 
-import by.it_academy.jd2.user_service.storage.entity.UserRole;
+import by.it_academy.lib.enums.UserRole;
 import by.it_academy.jd2.user_service.storage.entity.UserStatus;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 public class UserDTO {
-    @JsonProperty(index = 0)
-    private UUID id;
+    @JsonProperty(value = "uuid",index = 0)
+    private UUID userId;
 
     @JsonProperty(value = "dt_create", index = 1)
     private long dtCreate;
