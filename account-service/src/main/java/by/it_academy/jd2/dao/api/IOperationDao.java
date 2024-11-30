46d26ff03cc95e9ee120f5d97d055e4366f32105
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-11-30 16:32:33 +0300

    Добавлен метод findByOperationIdAndAccountId

--- account-service/src/main/java/by/it_academy/jd2/dao/api/IOperationDao.java
+++ account-service/src/main/java/by/it_academy/jd2/dao/api/IOperationDao.java
@@ -4,9 +4,16 @@ import by.it_academy.jd2.dao.entity.OperationEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface IOperationDao extends JpaRepository<OperationEntity, UUID> {
     Page<OperationEntity> findAllByAccountId(UUID id, Pageable pageable);
+
+
+    @Query("SELECT o FROM OperationEntity o WHERE o.id = :operationId AND o.account.id = :accountId")
+    OperationEntity findByOperationIdAndAccountId(@Param("operationId") UUID operationId,
+                                                  @Param("accountId") UUID accountId);
 }
