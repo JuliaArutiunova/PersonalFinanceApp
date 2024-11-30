e93955d18733f13482df5a23eda8789f5b7f2b5c
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-11-30 15:55:28 +0300

    Удалены UserEntity, UserDAO, UserService и IUserService из account-service

--- account-service/src/main/java/by/it_academy/jd2/dao/api/IUserDao.java
+++
@@ -1,9 +0,0 @@
-package by.it_academy.jd2.dao.api;
-
-import by.it_academy.jd2.dao.entity.UserIdEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface IUserDao extends JpaRepository<UserIdEntity, UUID> {
-}
