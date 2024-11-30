cb3efd6046db46079681ec0e876ab0251e28c107
Julia Arutiunova <j.arutiunova1@gmail.com>
2024-11-30 18:30:15 +0300

    Добавлен MapperConfig

---
+++ classifier-service/src/main/java/by/it_academy/jd2/classifier_service/config/MapperConfig.java
@@ -0,0 +1,35 @@
+package by.it_academy.jd2.classifier_service.config;
+
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Configuration
+public class MapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+
+        Converter<LocalDateTime, Long> timeConverter = new AbstractConverter<LocalDateTime, Long>() {
+            @Override
+            protected Long convert(LocalDateTime localDateTime) {
+                return localDateTime.toEpochSecond(ZoneOffset.UTC);
+            }
+        };
+        modelMapper.addConverter(timeConverter);
+
+        return modelMapper;
+    }
+}
